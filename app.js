7a7e7cffe9ebd45710989e3bbfb8f50ef4de9d55
himacks <karpinsky@chapman.edu>
2021-12-29 21:13:17 -0800

    updating files

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 'use strict'
 
-const switcher = document.querySelector('.btn');
+// Button to Change Light to Dark Theme
 
-switcher.addEventListener('click', function()
+const optionFields = ["date", "ticker", "callPut", "expiry", "numCons", "strike", "rr", "entry", "sl", "tp", "sold", "emotions", "tradeNotes"];
+
+const bgSwitcher = document.querySelector('#btnBgColor');
+
+var csvData = null; //csv data from text file
+
+var entryData = []; //csv data in array format
+
+bgSwitcher.addEventListener('click', function()
 {
     document.body.classList.toggle('dark-theme')
 
@@ -16,17 +24,77 @@ switcher.addEventListener('click', function()
     {
         this.textContent = "Light";
     }
-
-    console.log('current class name: ' + className);
 });
 
-var tbodyRef = document.getElementById('optionsTable').getElementsByTagName('tbody')[0];
-var newRow = tbodyRef.insertRow();
+// Button Functionality to Add Row to the Entry Table
 
-var newText = document.createTextNode('new row');
-var cellContent = "<td><div contenteditable></div></td>";
+const addRowBtn = document.querySelector('#btnAddRow');
 
-for (let i = 0; i < 13; i++) {
-    newRow.innerHTML += cellContent;
+addRowBtn.addEventListener('click', function()
+{
+    createTableRow();
+});
+
+var numRow = 0;
+
+function createTableRow(entryInfo)
+{
+    var tbodyRef = document.getElementById('optionsTable').getElementsByTagName('tbody')[0];
+
+    var newRow = tbodyRef.insertRow();
+    
+    var newRowContent = [];
+    
+    for (let i = 0; i < 13; i++) {
+
+        var content = '""';
+
+        if(entryInfo != undefined)
+        {
+            content = entryInfo[i];
+        }
+
+        var idName = optionFields[i] + numRow;
+
+        newRowContent.push(content);
+
+        var cellContent = '<td><input id=' + idName + ' type="text" value=' + content + ' /></td>';
+        newRow.innerHTML += cellContent;
+    }
+
+    entryData.push(newRowContent);
+
+    numRow += 1;
 }
 
+
+//Creates Table Based off Logged Data in entryLog.csv
+
+jQuery.get('http://127.0.0.1:5500/entryLog.csv', function(data) {
+    csvData = data.split(/\r?\n/);
+
+    for (let i = 1; i < csvData.length; i++)
+    {
+        createTableRow(csvData[i].split(','));
+    }
+});
+
+// On row change, updates information in csv file
+
+$(document).ready(function () {
+    
+    $(document).on("change", "#optionsTable :input", function() {
+        console.log(entryData);
+        var optionCell = $(this);
+        var optionCellValue = optionCell.val();
+        var unparsedID = optionCell.attr('id');
+        var cellRow = unparsedID.slice(-1);
+        var cellType = unparsedID.slice(0, unparsedID.length-1);
+        console.log("Type: " + cellType + ", Row: " + cellRow + ", Value: " + optionCellValue);
+
+        entryData[cellRow][optionFields.indexOf(cellType)] = optionCellValue;
+
+    });
+
+});
+ 
